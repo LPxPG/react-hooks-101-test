80c548c03c4ec6083b3cf2a73d66450a0fba9048
gitkuro <zebragel-cobaltblue@yahoo.co.jp>
2021-01-18 22:57:50 +0900

    refactor components

---
+++ src/components/Events.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Event from './Event'
+
+const Events = ({state, dispatch}) => {
+  return (
+    <>
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディ</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
+          {/* {
+
+            state.map((event, index) => { // => (...) : => {return ...multi-line...} の省略記法
+
+              const {id, title, body} = event
+              const handleClickDeleteButton = () => {
+                dispatch({type: 'DELETE_EVENT', id})
+              }
+
+              return <tr key={index}>
+                <td>{id}</td>
+                <td>{title}</td>
+                <td>{body}</td>
+                <td><button type="button" className="btn btn-danger" onClick={handleClickDeleteButton}>削除</button></td>
+              </tr>
+            })
+          } */}
+        </tbody>
+      </table>
+      </>
+  )
+}
+
+export default Events
