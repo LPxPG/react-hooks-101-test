027be6d3b37c60e1d6f6a64d471a39c2d999bc7a
gitkuro <zebragel-cobaltblue@yahoo.co.jp>
2021-01-18 15:08:07 +0900

    Implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-import React, {useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = (props) => {
-  // const initialStates = {
-  //   name: '',
-  //   price: 1000
-  // }
-  // const [name, setName] = useState(initialStates.name)
-  // const [price, setPrice] = useState(initialStates.price)
-
-  // const [name, setName] = useState(props.name)
-  // const [price, setPrice] = useState(props.price)
 
   const [state, setState] = useState(props)
-
   const {name, price} = state
 
-  // const reset = () => {
-  //   setPrice(props.price)
-  //   setName(props.name)
-  // }
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidUpdate.')
+  })
+  useEffect(() => {
+    console.log('This is like componentDidMount.')
+  }, [])
+  useEffect(() => {
+    console.log('This callback is for name only.')
+  }, [name])
+
+
+  const renderPeriod = () => {
+    console.log('renderPeriod renders  period.')
+    return '。'
+  }
 
   return (
     <>
-      {/*
-      <p>現在の{name}は、{price}です。</p>
-      <button onClick={()=>setPrice(price + 1)}>+1</button>
-      <button onClick={()=>setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={(e)=> setName(e.target.value)} />
-      */}
-
       <p>現在の{name}は、{price}です。</p>
       <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
       <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button>
